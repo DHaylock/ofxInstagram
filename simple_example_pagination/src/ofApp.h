76c2c34e95c4b9e0837012e44200ba388d3b5873
David Haylock <david.haylock@watershed.co.uk>
2015-03-04 09:45:12 +0000

    - Added standard example code

--- simple_example_pagination/src/ofApp.h
+++ simple_example_pagination/src/ofApp.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxInstagram.h"
 
 class ofApp : public ofBaseApp{
 
@@ -9,14 +10,13 @@ class ofApp : public ofBaseApp{
 		void update();
 		void draw();
 
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y );
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
+        void keyPressed(int key);
+        
+        void mouseDragged(int x, int y, int button);
+        void mousePressed(int x, int y, int button);
+        void mouseReleased(int x, int y, int button);
+        
+        ofxInstagram instagram;
+    
 		
 };
